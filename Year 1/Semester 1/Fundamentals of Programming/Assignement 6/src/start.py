45b1715cc2b773bb2f55c364f90fbd665826c4c5
RaresFM12 <rares.fodorca@stud.ubbcluj.ro>
2025-08-21 01:53:12 -0700

    fara fp1

--- Year 1/Semester 1/Fundamentals of Programming/Assignement 6/src/start.py
+++
@@ -1,9 +0,0 @@
-#
-# This module is used to invoke the program's UI and start it. It should not contain a lot of code.
-#
-from src.functions import initial_items, run_tests
-from src.ui import create_menu
-
-if __name__ == "__main__":
-    run_tests()
-    create_menu()
